fc55a9835c6c2fc984f65c50fac949de36c23fa2
leslieyjkim <yjblossom@gmail.com>
2023-10-24 16:31:53 +0000

    emptyString

---
+++ emptyString.js
@@ -0,0 +1,7 @@
+//write an if statement that checks if a given string is empty. If it is, print "String is Empty", otherwise, do nothing. 
+//'empty' and 'null' are different. 
+
+let str = "";
+if (str.length === 0) {
+  console.log("string is empty.");
+}
